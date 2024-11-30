947973004656353279fcdfcfaaa17389caaca403
shaghayegh <109871907+AdZeroNis@users.noreply.github.com>
2024-11-30 12:43:46 +0330

    Delete Weblog2 directory

--- Weblog2/js/scrollToUp.js
+++
@@ -1,8 +0,0 @@
-const btns = document.querySelectorAll('#scrollUpBtn');
-
-btns.forEach((btn) => {
-    btn.addEventListener('click', () => {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-    });
-});
